804d18cde628cfa42ccecb8631deafa9b8cc813b
roydonauyr <roydonauyr@gmail.com>
2024-05-22 17:05:46 +0800

    Added gitignore

--- MuteCompanion/frontend/App.js
+++ MuteCompanion/frontend/App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
